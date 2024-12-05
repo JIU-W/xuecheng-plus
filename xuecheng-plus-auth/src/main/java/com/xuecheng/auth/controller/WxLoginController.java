ed153c7f8719d45bf97ee5ccb27fc16263484d4b
JIU-W <2661727280@qq.com>
2024-12-05 20:13:17 +0800

    微信扫码认证

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/WxLoginController.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/WxLoginController.java
@@ -24,7 +24,8 @@ public class WxLoginController {
     private WxAuthService wxAuthService;
 
     //1.定义这个接口接收微信下发的授权码。
-    //前端用户扫了登录的微信二维码并且同意后，微信后台会向这个接口发送授权码，然后拿到用户信息，最后跳转到本项目的登录页面。
+    //前端用户扫了登录的微信二维码并且同意后，微信后台会向这个接口发送授权码(因为前端的wxsign.html里写了重定向到这个接口)，
+    //然后拿到用户信息，最后跳转到本项目的登录页面。
     @RequestMapping("/wxLogin")
     public String wxLogin(String code, String state) throws IOException {
 
@@ -37,7 +38,7 @@ public class WxLoginController {
         }
         String username = xcUser.getUsername();
         //5.重定向到浏览器自动登录
-        //进入我们的统一入口：自定义的UserServiceImpl实现类的loadUserByUsername方法
+        //进入我们的统一入口：自定义的UserServiceImpl实现类的loadUserByUsername方法 从而进入execute方法
         return "redirect:http://www.51xuecheng.cn/sign.html?username=" + username + "&authType=wx";
 
     }
